7d522c5192253a48bed19acc03c9058c2d86c185
DuyTana273 <duytana273@gmail.com>
2024-08-11 01:55:49 +0700

    Upload files

---
+++ C0624G1_Vu_Duy_Tan/module2/src/ss3/MethodAndArray/thuc_hanh/MaxInArray.java
@@ -0,0 +1,36 @@
+package ss3.MethodAndArray.thuc_hanh;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MaxInArray {
+    public static void main(String[] args) {
+        int size;
+        int[] array;
+        Scanner scanner = new Scanner(System.in);
+
+        do {
+            System.out.print("Enter a size (up to 20): ");
+            size = scanner.nextInt();
+            if (size > 20) {
+                System.out.println("Size should not exceed 20.");
+            }
+        } while (size > 20);
+
+        array = new int[size];
+        int max = array[0];
+        int maxIndex = 0;
+        for (int i = 0; i < array.length; i++) {
+            System.out.print("Enter element " + (i + 1) + ": ");
+            array[i] = scanner.nextInt();
+
+            if (array[i] > max) {
+                max = array[i];
+                maxIndex = i;
+            }
+        }
+        System.out.println("Property list: " + Arrays.toString(array));
+        System.out.println("The largest property value in the list is " + max + ", at position " + (maxIndex + 1));
+    }
+}
