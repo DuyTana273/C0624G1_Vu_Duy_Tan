8de995b6605a51917e08649eb67f28de8ad06477
DuyTana273 <duytana273@gmail.com>
2024-08-12 00:24:45 +0700

    Upload Flies

---
+++ C0624G1_Vu_Duy_Tan/module2/src/ss3/MethodAndArray/thuc_hanh/MinInArray_UseMethod.java
@@ -0,0 +1,41 @@
+package ss3.MethodAndArray.thuc_hanh;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MinInArray_UseMethod {
+    public static void main(String[] args) {
+        int size;
+        int[] array;
+        Scanner sc = new Scanner(System.in);
+
+        do {
+            System.out.print("Enter the size of the array: ");
+            size = sc.nextInt();
+            if (size > 10) {
+                System.out.println("Size does not exceed 20");
+            }
+        } while (size > 10);
+        array = new int[size];
+        int i = 0;
+        while (i < array.length) {
+            System.out.print("Enter element " + (i + 1) + ": ");
+            array[i] = sc.nextInt();
+            i++;
+        }
+        System.out.printf("%-20s%s", "Elements in array: ", Arrays.toString(array));
+
+        int index = minValue(array);
+        System.out.println(" The minimum value is " + array[index] + " at position " + index);
+    }
+
+    public static int minValue(int[] array) {
+        int index = 0;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < array[index]) {
+                index = i;
+            }
+        }
+        return index;
+    }
+}
