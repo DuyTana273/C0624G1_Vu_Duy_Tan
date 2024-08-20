da1f6a1bbdd8835cfb801d282e8f57a2865af1e2
DuyTana273 <duytana273@gmail.com>
2024-08-20 23:06:23 +0700

    Upload Files

---
+++ C0624G1_Vu_Duy_Tan/module2/src/ss9_AutomatedTestingAndTDD/thuc_hanh/SimplelCalculator/SimpleCalculator.java
@@ -0,0 +1,11 @@
+package ss9_AutomatedTestingAndTDD.thuc_hanh.SimplelCalculator;
+
+public class SimpleCalculator {
+    public static int add(int first, int second){
+        return first + second;
+    }
+
+    public static int sub(int first, int second){
+        return first - second;
+    }
+}
