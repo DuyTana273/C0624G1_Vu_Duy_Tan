0c9a71fbcdf7e7021091a35f9be12d1c9087e2fb
DuyTana273 <duytana273@gmail.com>
2024-08-28 00:44:11 +0700

    Upload Files

---
+++ C0624G1_Vu_Duy_Tan/module2/src/ss11_StackAndQueue/thuc_hanh/MyLinkedListQueue/Node.java
@@ -0,0 +1,11 @@
+package ss11_StackAndQueue.thuc_hanh.MyLinkedListQueue;
+
+class Node {
+    public int key;
+    public Node next;
+
+    public Node(int key) {
+        this.key = key;
+        this.next = null;
+    }
+}
